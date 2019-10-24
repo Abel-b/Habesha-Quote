6745cf633e4f7424835f4b419d7d9da22876ff51
Abel <abelberhane011@gmail.com>
2019-10-24 13:58:11 +0300

    adds quote ts file

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -7,6 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quotes = [
+    new Quote(2, 'Charles', 'I learnt how to learn', 'admin', new Date(1950, 3, 23)),
+    new Quote(1, 'Moringa School', 'Code your problems!', 'admin', new Date(1990, 5, 10)),
+    new Quote(3, 'Abel Berhane', 'Cheap is expensive', 'admin', new Date(2012, 8, 17)),
+
+  ]
+
+  showDescription= false;
+
+    addNewQuote(quote) {
+      let quoteLength = this.quotes.length;
+      quote.id = quoteLength + 1;
+      quote.completeDate = new Date(quote.completeDate)
+      this.quotes.push(quote)
+    }
   constructor() { }
 
   ngOnInit() {
