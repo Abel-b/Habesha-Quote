63bb07a1158cbdbe855b4b9657d16bc6b28769d2
Abel <abelberhane011@gmail.com>
2019-10-26 11:42:41 +0300

    edits warning messsage

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -9,28 +9,3 @@ export class Quote {
         public completeDate: Date) {
     }
 }
-    export class Quote1 {
-
-    showDescription1 = false;
-
-    constructor(
-        public id: number,
-        public name: string,
-        public description: string,
-        public submitter: string,
-        public completeDate: Date) {
-    }
-}
-export class Quote2 {
-
-    showDescription2 = false;
-
-    constructor(
-        public id: number,
-        public name: string,
-        public description: string,
-        public submitter: string,
-        public completeDate: Date) {
-    }
-
-}
